7e02687a3d633f6fbe1b6d981fd833dbf17651ee
Ali <konnection.official@gmail.com>
2025-04-09 15:19:38 -0400

    More Vercel fixes

--- src/app/api/get-plants/route.ts
+++ src/app/api/get-plants/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from "next/server";
 import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
 
 // DynamoDB configuration
-const dynamoDB = new DynamoDBClient({
-  region: "us-east-2",
+export const dynamoDB = new DynamoDBClient({
+  region: process.env.AWS_REGION,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
