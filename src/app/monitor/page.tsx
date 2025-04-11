4158177c30fb38b997a0e4abb0965716aa43400e
Ali <konnection.official@gmail.com>
2025-04-11 17:09:24 -0400

    More integration fixes

--- src/app/monitor/page.tsx
+++ src/app/monitor/page.tsx
@@ -15,7 +15,7 @@ import '../styles.css';
 */
 
 const dynamoDB = new DynamoDBClient({
-  region: process.env.AWS_REGION,
+  region: "us-east-2",
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
